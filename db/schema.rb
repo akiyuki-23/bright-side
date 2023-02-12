25f1b2d319fdc8300b20496dab18661ebaf27f0d
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2023-02-12 23:09:07 +0900

    カラム情報の記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_140453) do
+ActiveRecord::Schema.define(version: 2023_02_12_135010) do
+
+  create_table "good_founds", charset: "utf8", force: :cascade do |t|
+    t.date "execution_date", null: false
+    t.string "title", null: false
+    t.text "event_detail", null: false
+    t.integer "event_category", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_good_founds_on_user_id"
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2023_01_18_140453) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "good_founds", "users"
 end
