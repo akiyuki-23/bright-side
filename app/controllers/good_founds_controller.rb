017689396f14ec2d40234df79e9792b3cec03381
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2023-02-20 23:33:51 +0900

    編集機能の実装

--- app/controllers/good_founds_controller.rb
+++ app/controllers/good_founds_controller.rb
@@ -1,5 +1,6 @@
 class GoodFoundsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @good_founds = GoodFound.order('created_at DESC')
   end
@@ -11,7 +12,7 @@ class GoodFoundsController < ApplicationController
   def create
     @good_found = GoodFound.new(good_found_params)
     if @good_found.save
-      redirect_to root_path
+      redirect_to root_path, notice: 'You had a good day!! いい日ですね！'
     else
       render :new
     end
@@ -21,9 +22,27 @@ class GoodFoundsController < ApplicationController
     @good_found = GoodFound.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @good_found.update(good_found_params)
+    redirect_to good_found_path(@good_found.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def good_found_params
     params.require(:good_found).permit(:execution_date, :title, :event_detail, :category_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @good_found = GoodFound.find(params[:id])
+    return if @good_found.user == current_user
+
+    redirect_to root_path
   end
 end
