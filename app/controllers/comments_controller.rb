dda742249a91f633d1286d9c8f1e746d2981d05f
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2023-03-05 23:18:45 +0900

    コントローラーの変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,34 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
+
   def create
     @comment = Comment.create(comment_params)
     if @comment.save
-      redirect_to good_found_path(@comment.good_found)
+      respond_to do |format|
+        format.html { redirect_to good_found_path(@comment.good_found) }
+        format.json { render json: { comment: @comment, success: true } }
+      end
     else
       @good_found = @comment.good_found
       @comments = @good_found.comments
-      render template: "good_founds/show"
+      respond_to do |format|
+        format.html { render template: "good_founds/show" }
+        format.json { render json: { errors: @comment.errors.full_messages, success: false } }
+      end
     end
   end
 
   def destroy
     @comment = Comment.find_by(params[:id], good_found_id: params[:good_found_id])
     @comment.destroy
-    redirect_to good_found_path(@comment.good_found)
+    respond_to do |format|
+      format.html { redirect_to good_found_path(@comment.good_found) }
+      format.json { render json: { success: true } }
+    end
   end
 
-
   private
+
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, good_found_id: params[:good_found_id])
   end
