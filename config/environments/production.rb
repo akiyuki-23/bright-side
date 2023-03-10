454a29180dd9fa4c582cb960549372a5615e970f
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2023-03-10 23:42:58 +0900

    本番環境設定の実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,10 +40,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Mount Action Cable outside main process or domain.
+  ActionCable.server.config.disable_request_forgery_protection = true
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://bright-side.onrender.com/cable"
+  config.action_cable.allowed_request_origins = [ 'https://bright-side.onrender.com', 'https://bright-side.onrender.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
